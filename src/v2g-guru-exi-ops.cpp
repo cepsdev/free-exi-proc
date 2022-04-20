9fda2f2762887512ca534d364cb6373b3e073b84
Tomas Prerovsky <cepsdev@hotmail.com>
2022-04-20 11:24:10 +0200

    Add Test/skeleton implementation for renaming operation.

---
+++ src/v2g-guru-exi-ops.cpp
@@ -0,0 +1,29 @@
+/*
+    v2g-guru-exi - an EXI (= "Efficient XML Interchange") processor
+    Copyright (C) 2022 Tomas Prerovsky <cepsdev@hotmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
+#include "v2g-guru-exi.h"
+
+namespace v2g_guru_exi{
+    bool Grammar::rename_non_termial(string from , string to){
+        if (from.length() == 0 || to.length() == 0) return false;
+        return true;
+    }
+
+}
+
