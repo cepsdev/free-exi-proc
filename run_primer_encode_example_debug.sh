ae11f74e15fdc0d5cfb1a519f9372a1252d80e8a
Tomas Prerovsky <cepsdev@hotmail.com>
2022-10-10 15:43:49 +0200

    [Model][EXIPrimer] Intro+Concepts.

--- run_primer_encode_example_debug.sh
+++ run_primer_encode_example_debug.sh
@@ -4,4 +4,4 @@ LD_LIBRARY_PATH=$(pwd)/bin:$LD_LIBRARY_PATH ceps \
 model/exi.events.ceps \
 model/exi.grammars.ceps \
 model/exi.primer.example.ceps \
- --pluginlibv2g-guru-exi-plugin.so --pr --format ansi
+ --pluginlibv2g-guru-exi-plugin.so --ppe --format ansi
